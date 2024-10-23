43d7c72f55ec432c3bf5b49c9cd4d4ca8c974a86
Gayan65 <gayan65728@yahoo.com>
2024-10-24 00:58:20 +0300

    init

---
+++ src/main/java/com/cricket_bash/team_players/TeamPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.cricket_bash.team_players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamPlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket_bash/team_players/TeamPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket_bash.team_players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
